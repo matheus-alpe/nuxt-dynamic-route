f73b9eff15c10ea8ca5697b0f815270083d42a1f
Matheus Alves Pereira <matttalves@gmail.com>
2024-03-08 18:47:45 -0300

    feat: fallback page

---
+++ pages/[...slug].vue
@@ -0,0 +1,13 @@
+<template>
+  <h1>
+    Not found `{{
+      Array.isArray($route.params.slug)
+        ? $route.params.slug.join("/")
+        : $route.params.slug
+    }}` page
+  </h1>
+
+  <nav>
+    <NuxtLink to="/">Home</NuxtLink>
+  </nav>
+</template>
